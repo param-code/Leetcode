409acaec97ef93fcc8748b2d4e37ce271758d1d9
Paramveer Singh <109423225+param-code@users.noreply.github.com>
2024-11-07 22:27:48 +0530

    Time: 4 ms (37.03%), Space: 12 MB (7.78%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,16 +1,9 @@
 class Solution {
 public:
-    long long reversex(long long x){
-        long long ans = 0;
-        while(x > 0){
-            ans = (ans * 10) + (x % 10);
-            x /= 10;
-        }
-        return ans;
-    }
     bool isPalindrome(int x) {
-        if(x < 0)return false;
-        long long rev = reversex(x);
-        return rev == x;
+        string s = to_string(x);
+        string n = s;
+        reverse(begin(n),end(n));
+        return s == n;
     }
 };
